07df364b5f4580352c8618585cb721f565a8b6a2
anonymia <anonymia>
2025-11-02 16:19:12 +0700

    fix merge pattern

--- release-rc.config.js
+++ release-rc.config.js
@@ -9,9 +9,9 @@ module.exports = {
       {
         preset: "conventionalcommits",
         parserOpts: {
-          headerPattern: /^(\w+)(\(.+\))?!?: (.+)$/,
+          headerPattern: /^\s*\*?\s*(\w+)(\(.+\))?!?: (.+)$/,
           headerCorrespondence: ["type", "scope", "subject"],
-          mergePattern: /^(.+) #(\d+)$/,
+          mergePattern: /^.+ \(#(\d+)\)$/,
           mergeCorrespondence: ['id'],
           noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
           revertPattern: /^revert:\s([\s\S]*)$/i,
