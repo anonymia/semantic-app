d649aa96484f0b5b4976af0259830072ed0c1f0d
anonymia <anonymia>
2025-11-11 15:31:40 +0700

    fix: remove unused config keys

--- .github/scripts/release-rc.config.js
+++ .github/scripts/release-rc.config.js
@@ -10,10 +10,6 @@ module.exports = {
     // No publish/release plugins here!
   ],
   tagFormat: "v${version}",
-  // Don't generate release notes or changelogs, don't publish npm/github releases
-  generateNotes: false,
-  prepare: false,
-  publish: false,
-  success: false,
-  fail: false,
+  // Don't generate release notes or changelogs
+  generateNotes: false
 };
